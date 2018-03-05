036d90372adc881bfab378257e6834e42bbed762
Kavilan Nair <kavilann@gmail.com>
2018-03-05 21:00:37 +0200

    Removed unnecessary comments

--- mainRoutes.js
+++ mainRoutes.js
@@ -12,7 +12,3 @@ mainRouter.get('/about', function(req, res) {
 });
 
 module.exports = mainRouter;
-
-// app.use('/', mainRouter);
-// app.listen(3000);
-// console.log("Express server running on port 3000");
